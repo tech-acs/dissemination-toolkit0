5605db29c3568be91b9c401d4575b98fdc9cefeb
Nahom <nahomt@amestsantim.com>
2024-11-02 09:46:42 +0300

    Multilang capability (en, fr, and pt). Actual translations to be done...

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'enforce_2fa' => RedirectIf2FAEnforced::class
         ]);
+        $middleware->appendToGroup('web', [
+            \App\Http\Middleware\Language::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
